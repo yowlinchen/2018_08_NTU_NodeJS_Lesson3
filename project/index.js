b3a7eb79001eb00c41824663e0b31e2a1cf33379
yowlinchen <terry.y.chen@intel.com>
2018-08-12 17:10:35 +0800

    chnage on server side

--- project/index.js
+++ project/index.js
@@ -4,11 +4,17 @@ let express = require("express");
 let app = express();
 app.use(express.static("www"));
 
-app.get("/calculate", function (req, res) {
+app.post("/calculate", function (req, res) {
     let number = req.query.number;
     let result = number * number;
     res.send("Answer: " + result);
 });
+
+// app.get("/calculate", function (req, res) {
+//     let number = req.query.number;
+//     let result = number * number;
+//     res.send("Answer: " + result);
+// });
 app.listen(3000, function () {
     console.log("Server Started");
 });
