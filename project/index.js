4a9548642fe817ad2b6379558bca6d0db3ac31bc
yowlinchen <terry.y.chen@intel.com>
2018-08-12 18:09:14 +0800

    default to some cleaner state

--- project/index.js
+++ project/index.js
@@ -11,17 +11,6 @@ app.use(parser.urlencoded({
     extended: true
 }));
 
-app.post("/calculate", function (req, res) {
-    let number = req.body.number;
-    let result = number * number;
-    res.send("Answer: " + result);
-});
-
-// app.get("/calculate", function (req, res) {
-//     let number = req.query.number;
-//     let result = number * number;
-//     res.send("Answer: " + result);
-// });
 app.listen(3000, function () {
     console.log("Server Started");
 });
